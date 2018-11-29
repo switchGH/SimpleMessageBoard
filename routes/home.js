2988c793f46aa7f6c38a13fba28e711c96b6c62f
switch <shg18.sk1.hr10@gmail.com>
2018-11-30 00:52:25 +0900

    ホームページの処理

---
+++ routes/home.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var router = express.Router();
+
+var knex = require('knex')({
+  dialect: 'sqlite3',
+  connection: {
+    filename: 'board_data.sqlite3'
+  },
+  useNullAsDefault: true
+});
+
+var Bookshelf = require('bookshelf')(knex);
+
+Bookshelf.plugin('pagination');
+
+var User = Bookshelf.Model.extend({
+  tableName: 'users'
+});
+
+var Message = Bookshelf.Model.extend({
+  tableName: 'message',
+  hasTimestamps: true,
+  user: function(){
+    return this.belogsTo(User);
+  }
+})
+
+router.get('/', function(req, res, next) {
+  res.redirect('/home/' + req.params.id + '/1');
+});
+
+router.get('/:id/:page', function(req, res, next) {
+  var id = req.params.id;
+  id *= 1;
+  var pg = req.params.page;
+  pg *= 1;
+  if(pg < 1){
+    pg = 1;
+  }
+  new Message().orderBy('correct_at', 'DESC')
+    .where('user_id', '=', id)
+    .fetchPage({page:pg, pageSize: 10, withRelated: ['user']})
+    .then(function(collection){
+      var data = {
+        title: 'miniBoard',
+        login: req.session.login,
+        user_id: id,
+        collection: collection.toArray(),
+        pagination: collection.pagination
+      };
+      res.render('home', data);
+    }).catch(function(err){
+      res.status(500).json({error: true, data: {message: err.message}});
+    });
+});
+
+module.exports = router;
+
+
+
